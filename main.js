91155c7235fe3ead3d71bfa8dfb99db6649c9886
adeoke <adesmail100@gmail.com>
2021-02-14 12:13:19 +0000

    template literals

--- main.js
+++ main.js
@@ -8,3 +8,24 @@
 
 let age = 30;
 console.log(age);
+
+//some primitive data types
+//String, boolean, Number, null, undefined
+
+const str = 'a string single or double quotes';
+const isBool = true;
+const aNumber = 14;
+const anotherNumber = 9.79;
+const aNull = null;
+const anUndefined = undefined;
+let alsoUndefined;
+
+console.log(typeof alsoUndefined);
+console.log(typeof isBool);
+console.log(typeof aNull);
+
+//template literals
+const message = `my age is ${aNumber} some other junk is ${anUndefined}`;
+
+console.log(message);
+

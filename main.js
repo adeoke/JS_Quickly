270e64b0142e4e1694ffe32cc7e8bf720ed8c69e
adeoke <adesmail100@gmail.com>
2021-02-14 12:00:22 +0000

    basic comments, introducing var.let/const

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+// For variables no longer use ****"var"*****, this is because it has global scope and can cause conflict
+
+//instead use "let" and "const", which have block level scope, which came from es6.
+
+//always use const unless you know you are goign to reassign the variable. It's more secure. 
+//const must be assigned on creation.
+
+
+let age = 30;
+console.log(age);
